dd2bfb4b516a2c0f64cc267efe1ccfb3cede0fcb
fredfrancia1991 <fredfrancia@icloud.com>
2025-07-10 00:34:43 +0200

    fix: use relative import for Header and Footer

--- src/app/admin/utilisateurs/[uid]/page.tsx
+++ src/app/admin/utilisateurs/[uid]/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
 interface Props {
   params: {
     uid: string;
